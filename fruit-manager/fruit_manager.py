bdd8e646dac61eee69ac7b7598fa5bc2ad6e511d
Bilal <bilal@Host-001.lan>
2026-01-20 21:35:23 +0100

    ajout de la fonctionnalite de tresorerie

--- fruit-manager/fruit_manager.py
+++ fruit-manager/fruit_manager.py
@@ -2,39 +2,55 @@
 
 import json
 
-def ouvrir_inventaire(path='inventaire.json'):
+def ouvrir_inventaire(path='data/inventaire.json'):
     with open(path, 'r', encoding='utf-8') as f:
         inventaire = json.load(f)
     return inventaire
 
-def ecrire_inventaire(inventaire, path='inventaire.json'):
+def ecrire_inventaire(inventaire, path='data/inventaire.json'):
     with open(path, 'w', encoding='utf-8') as f:
-        json.dump(f, path, ensure_ascii=False, indent=4)
+        json.dump(inventaire, f, ensure_ascii=False, indent=4)
 
-inventaire = ouvrir_inventaire('data/inventaire.json')
+def ouvrir_tresorerie(path='data/tresorerie.txt'):
+    with open(path, 'r', encoding='utf-8') as f:
+        tresorerie = json.load(f)
+    return tresorerie
+
+def ecrire_tresorerie(tresorerie, path='data/tresorerie.txt'):
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(tresorerie, f, ensure_ascii=False, indent=4)
+
+def afficher_tresorerie(tresorerie):
+    print(f"Tresorerie actuelle actuel : {tresorerie:.2f} $")
 
 def afficher_inventaire(inventaire):
     print("Inventaire actuel de la plantation")
     for fruit, quantite in inventaire.items():
         print(f"- {fruit.capitalize()} : {quantite} unités")
 
-afficher_inventaire(inventaire)
-
 def recolter(inventaire, fruit, quantite):
     inventaire[fruit] = inventaire.get(fruit, 0) + quantite
     print(f"Récolté {quantite} {fruit} supplémentaires")
 
-recolter(inventaire, "bananes", 10)
-
-afficher_inventaire(inventaire)
-
-def vendre(inventaire, fruit, quantite):
+def vendre(inventaire, fruit, quantite, tresorerie):
     if inventaire.get(fruit, 0) >= quantite:
         inventaire[fruit] -= quantite
+        tresorerie += 1 * quantite
         print(f"Vendu {quantite} {fruit}")
+        return (inventaire, tresorerie)
     else:
         print("Pas assez de {fruit} pour vendre {quantite} unités")
 
-vendre(inventaire, "bananes", 5)
 
-afficher_inventaire(inventaire)
+if __name__ == "__main__":
+    inventaire = ouvrir_inventaire('data/inventaire.json')
+    tresorerie = ouvrir_tresorerie('data/tresorerie.txt')
+    afficher_tresorerie(tresorerie)
+    afficher_inventaire(inventaire)
+
+    recolter(inventaire, "bananes", 10)
+    inventaire, tresorerie = vendre(inventaire, "bananes", 5, tresorerie)
+
+    ecrire_inventaire(inventaire)
+    ecrire_tresorerie(tresorerie)
+
